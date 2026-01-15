4a1ed2b701f7e1f600360a16504bfa840e2e28f9
gayatripawar2004 <gayatripawar1880@gmail.com>
2026-01-15 12:59:31 +0530

    Change MySQL connection settings
    Updated MySQL connection details to use Clever Cloud service.

--- connection.js
+++ connection.js
@@ -3,11 +3,10 @@ var util = require("util");
 var url = require("url");
 
 const conn = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "root",
-    database:"sakhi_spark",
-    port: "3307"
+    host:"biuv65ldy5ygvxlttmhp-mysql.services.clever-cloud.com",
+    user: "uwpq8ggyq5volmtm",
+    password: "kKNaVEn7eH36u3ThMsXe",
+    database:"biuv65ldy5ygvxlttmhp"
 })
 
 const exe = util.promisify(conn.query).bind(conn);
