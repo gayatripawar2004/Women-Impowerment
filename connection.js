5109f50dd730fec390f711f3c83329c6b8b17276
gayatripawar2004 <gayatripawar1880@gmail.com>
2026-01-20 15:04:52 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,11 +3,10 @@ var util = require("util");
 var url = require("url");
 
 const conn = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "root",
-    database:"sakhi_spark",
-    port: "3307"
+    host:"bs0cflawbzpbcqdn1eoy-mysql.services.clever-cloud.com",
+    user: "ue2dkblryxvutm0v",
+    password: "0EmD0TFnUhZlczj2bo2G",
+    database:"bs0cflawbzpbcqdn1eoy"
 })
 
 const exe = util.promisify(conn.query).bind(conn);
