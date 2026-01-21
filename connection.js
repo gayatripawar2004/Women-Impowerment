24249c4b6118799216e36cf323c1d6f5c552e058
gayatripawar2004 <gayatripawar1880@gmail.com>
2026-01-21 11:22:14 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,14 +1,15 @@
-var mysql = require("mysql2");
-var util = require("util");
-var url = require("url");
+const mysql = require("mysql2");
+const util = require("util");
 
-const conn = mysql.createConnection({
-    host:"bs0cflawbzpbcqdn1eoy-mysql.services.clever-cloud.com",
+const pool = mysql.createPool({
+    host: "bs0cflawbzpbcqdn1eoy-mysql.services.clever-cloud.com",
     user: "ue2dkblryxvutm0v",
     password: "0EmD0TFnUhZlczj2bo2G",
-    database:"bs0cflawbzpbcqdn1eoy"
-})
+    database: "bs0cflawbzpbcqdn1eoy",
+   
+});
 
-const exe = util.promisify(conn.query).bind(conn);
+// promisify pool.query
+const exe = util.promisify(pool.query).bind(pool);
 
 module.exports = exe;
